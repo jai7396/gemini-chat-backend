af0369c8d44cd4caf25c0c040d6c30b3704000ae
jai7396 <janardhan.vodugu@gmail.com>
2025-08-02 14:58:50 +0530

    Update db_conn.py

--- app/db_conn.py
+++ app/db_conn.py
@@ -1,6 +1,9 @@
-import os
 import psycopg2
 
-DATABASE_URL = os.getenv("DATABASE_URL")
-
-conn = psycopg2.connect(DATABASE_URL)
+conn = psycopg2.connect(
+    host="localhost",
+    port=5432,
+    user="postgres",
+    password="Pandu@123",
+    database="gemini_backend"
+)
